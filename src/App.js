fc9fa9ca2451df5c7cfc09c14ee0a5536d5b6879
Vanessa Pacheco <nesspachecod@gmail.com>
2021-10-03 18:40:04 -0500

    feat: move to next question

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
 import './App.css';
 import Question from './Question';
 import { questions } from './questions';
+import { useState } from 'react';
+
 
 function App () {
+  const [currentQuestionId, setCurrentQuestionId] = useState(1);
+  
+  const handleGoToNextQuestion = () => {
+    setCurrentQuestionId(currentQuestionId + 1);
+  }
+  
   return (
     <div className="App">
-      {questions.map(ele =>
-        <Question text={ele.question} answer={ele.answer} />
+      {questions.filter(ele => ele.id === currentQuestionId).map(ele =>
+        <Question 
+          answer={ele.answer} 
+          onGoToNextQuestion={handleGoToNextQuestion} 
+          text={ele.question} 
+        />
       )}
     </div>
   );
