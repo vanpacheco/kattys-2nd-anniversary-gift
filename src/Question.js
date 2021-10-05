06f6ef5ea757b25e87b3d92ce4fe7264ef6ecd8d
Vanessa Pacheco <nesspachecod@gmail.com>
2021-10-04 22:02:03 -0500

    feat: add error messages

--- src/Question.js
+++ src/Question.js
@@ -1,38 +1,53 @@
 import Button from '@mui/material/Button';
-import Paper from '@mui/material/Paper';
 import Slide from '@mui/material/Slide';
 import TextField from '@mui/material/TextField';
 import Typography from '@mui/material/Typography';
 import { useState } from 'react';
 
-const Question = ({ answer, onGoToNextQuestion, text }) => {
+const Question = ({ answer, helperTexts, onGoToNextQuestion, text }) => {
     const [userAnswer, setUserAnswer] = useState('');
+    const [hasError, setHasError] = useState(false);
+    const [numberOfTries, setNumberOfTries] = useState(0);
     
     const handleChange = e => {
+        if (hasError) setHasError(false);
         setUserAnswer(e.target.value);
     };
 
     const checkIfAnswerIsRight = () => {
-        if ( answer === userAnswer ) {
+        setNumberOfTries(numberOfTries + 1);
+        if (answer === userAnswer 
+            || answer.toLowerCase() === userAnswer.toLowerCase()) {
             setUserAnswer('');
             onGoToNextQuestion();
-       } 
+        } else {
+            setHasError( true );
+            onGoToNextQuestion();
+        }
     }
 
     return (
-        <Slide direction="up" in appear>
-            <Paper elevation={4}>
-                <Typography>{text}</Typography>
-                <TextField
-                    id="standard-basic"
-                    label="Standard"
-                    onChange={handleChange}
-                    value={userAnswer}
-                    variant="standard"
-                />
-                <Button onClick={checkIfAnswerIsRight}>Enviar</Button>
-            </Paper>
-        </Slide>
+        <form onSubmit={checkIfAnswerIsRight}>
+            <Slide direction="up" in appear timeout={500}>
+                <div className="question-container">
+                    <div className="question">
+                        <Typography variant="h4">{text}</Typography>
+                        <TextField
+                            autoComplete="off"
+                            error={hasError}
+                            helperText={hasError && 
+                                (helperTexts[numberOfTries - 1] || 'Nope :(')
+                            }
+                            id="standard-basic"
+                            onChange={handleChange}
+                            value={userAnswer}
+                            variant="filled"
+                        />
+                        <Button variant="contained" type="submit">Enviar</Button>
+                    </div>
+                </div>
+            </Slide>
+        </form>
     );
 }
 
