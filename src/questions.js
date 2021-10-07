91c5213c5e5f5aa6ce0881a583d888a10bcdd1b4
Vanessa Pacheco <nesspachecod@gmail.com>
2021-10-06 21:33:01 -0500

    feat: add message at the end

--- src/questions.js
+++ src/questions.js
@@ -1,57 +1,73 @@
 export const questions = [
     {
+        helperTexts: ['Ombe! Sea serio, vale mía! Piensa bien...'],
         id: 1, 
-        question: "En qué año nos conocimos?",
+        question: "¿En qué año nos conocimos?",
         answer: "2013"
     },
     {
+        helperTexts: ['Está tan fácil que no debiste haber visto este mensaje'],
         id: 2, 
-        question: "En dónde nos conocimos?",
+        question: "¿En dónde nos conocimos?",
         answer: "SENA"
     },
     {
+        helperTexts: [],
         id: 3, 
-        question: "Qué edad tenía cuando me conociste?",
+        question: "¿Qué edad tenía cuando me conociste?",
         answer: "17"
     },
     {
+        helperTexts: ['👩', 'Bueno, una pista... rgb y sin espacios'],
         id: 4, 
-        question: "De qué color tenía el cabello cuando me conociste?",
-        answer: "rgb(255, 255, 0)"
+        question: "¿De qué color tenía el cabello cuando me conociste?",
+        answer: "rgb(255,255,0)"
     },
     {
+        helperTexts: ['Ombe! Sea serio, vale mía! Piensa bien...'],
         id: 5, 
-        question: "Cuál fue el primer regalo que me hiciste?",
+        question: "¿Cuál fue el primer regalo que me hiciste?",
         answer: "La Fldsmdfr"
     },
     {
+        helperTexts: ['rgb...'],
         id: 6, 
-        question: "De qué color me pinté el cabello el día del abrazo eterno en la capilla?",
-        answer: "rgb(0, 0, 0)"
+        question: "¿De qué color me pinté el cabello el día del abrazo eterno en la capilla?",
+        answer: "rgb(0,0,0)"
     },
     {
+        helperTexts: ['😲', '😢', '😭', 'Asumiré que solo quieres ver todos los mensajes de error'],
         id: 7, 
-        question: "Qué día te enteraste que ibas a ser papá?",
+        question: "¿Qué día te enteraste que ibas a ser papá?",
+        placeholder: 'DD/MM/YYYY',
         answer: "18/10/2017"
     },
     {
+        helperTexts: ['😲', '😢', '😭'],
         id: 8, 
-        question: "Qué día nos convertimos en papás?",
+        question: "¿Qué día nos convertimos en papás?",
+        placeholder: 'DD/MM/YYYY',
         answer: "28/05/2018"
     },
     {
+        helperTexts: ['😲', '😢', '😭'],
         id: 9, 
-        question: "Qué día me propusiste matrimonio?",
+        question: "¿Qué día me propusiste matrimonio?",
+        placeholder: 'DD/MM/YYYY',
         answer: "18/09/2020"
     },
     {
+        helperTexts: ['😲', '😢', '😭'],
         id: 10, 
-        question: "Qué día nos casamos?",
+        question: "¿Qué día nos casamos?",
+        placeholder: 'DD/MM/YYYY',
         answer: "10/10/2020"
     },
     {
+        helperTexts: ['😲', '😢', '😭', 'Asumiré que solo quieres ver todos los mensajes de error'],
         id: 11, 
-        question: "Cuándo fue la última vez que nos vimos en persona?",
+        question: "¿Cuándo fue la última vez que nos vimos en persona?",
+        placeholder: 'DD/MM/YYYY',
         answer: "19/02/2021"
     }
 ]
